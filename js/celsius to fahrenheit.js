031e5c772ec39f0165658c3f1fa51da31e4f1081
Javier Jimenez and John Parsons <javier@>
2017-08-07 11:37:50 -0700

    celsius to fahrenheit calculator

---
+++ js/celsius to fahrenheit.js
@@ -0,0 +1,7 @@
+var multiplay = function(number1) {
+  return number1;
+};
+
+var number1 = parseFloat(prompt("Enter teparature in celsius:"));
+var result = multiplay(number1*1.8+32);
+alert(result);
