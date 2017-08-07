4af1d9c5538c225e5fb01b22130cbbcf7f7b4ce8
Javier Jimenez and John Parsons <john@>
2017-08-07 14:41:28 -0700

    added jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+});
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+// alert(result);
+//
+//
+
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+// alert(result)};
+
+
 
 
 // var multiply = function(number1, number2) {
